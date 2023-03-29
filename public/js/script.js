29a0021b137459dc530b2bc62a64464876b305b4
JanSztefek <sztefek.jan@seznam.cz>
2023-03-29 12:10:59 +0200

    homepage slider update

--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,13 @@ slideTime = 10000;
 pos = 1;
 
 $(document).ready(function(){
-    backgroundSlide = $('#slideshow__main').lightSlider({
+    slider = $('#slideshow__main').lightSlider({
         gallery: false,
         item: 1,
         controls: false,
         list: false,
-        loop:true,
-        pager: true,
-        thumbItem: 1,
+        pager: false,
+        thumbItem: 7,
         enableDrag: true,
         enableTouch: true,
         autoWidth: false,
@@ -17,63 +16,93 @@ $(document).ready(function(){
         auto: true,
         slideMargin: 300,
         pause: slideTime,
-        onBeforeSlide: function(){
-        }
+        loop:true,
+        onAfterSlide: function(){
+            changeMenuSliderPosition();
+        },
+      
     });
-    backgroundSlide = $('.slideshowflex').lightSlider({
+
+    slider_navigation = $('#slideshow__mainnavigation').lightSlider({
         gallery: false,
-        item: 3,
         controls: false,
         list: false,
-        loop:false,
-        pager: true,
-        thumbItem: 1,
-        enableDrag: false,
-        enableTouch: false,
-        auto: false,
-        slideMargin: 50,
+        pager: false,
+        enableDrag: true,
+        item: 6,
+        enableTouch: true,
+        autoWidth: true,
+        adaptiveHeight: true,
+        auto: true,
         pause: slideTime,
-        loop:false,
+        slideMargin: 0,
+        centerSlide:true,  
+        loop: true,
+        
+    });
+
+
+    $('#slideshowflex').lightSlider({
+        gallery: false,
+        item: 7,
+        controls: false,
+        list: false,
+        loop:true,
+        pager: false,
+        thumbItem: 1,
+        enableDrag: true,
+        enableTouch: true,
+        auto: true,
+        slideMargin: 50,
+        pause: 2600,
+        adaptiveHeight: true,
         responsive:[
             {
-                breakpoint: 1380,
+                breakpoint:1080,
                 settings: {
-                    item: 2,
-                    loop:true,
-                    enableDrag: true,
-                    enableTouch: true,
-                    controls: true,
-                }
+                    item:5,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
             },
             {
-                breakpoint: 800,
+                breakpoint:768,
                 settings: {
-                    loop:true,
-                    enableDrag: true,
-                    enableTouch: true,
-                    item: 1,
-                    controls: true,
-                }
-            }
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:550,
+                settings: {
+                    item:2,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
         ],
+
         onBeforeSlide: function(){
         }
     });
 });
 
 function slideTo(pos){
-    backgroundSlide.goToSlide(pos);
-    textSlide.goToSlide(pos);
-    $('#buttons__slide button.active').removeClass('active');
-    $('#buttons__slide button:nth-child('+pos+')').addClass('active');
+    slider.goToSlide(pos);
+    slider_navigation.goToSlide(pos);
 }
 
 function slidePrev(){
     backgroundSlide.goToPrevSlide()
-    textSlide.goToPrevSlide();
+    slider_navigation.goToPrevSlide();
 }
 
 function slideNext(){
     backgroundSlide.goToNextSlide()
-    textSlide.goToNextSlide();
+    slider_navigation.goToNextSlide();
+}
+
+function changeMenuSliderPosition(){
+    slider_navigation.goToSlide(slider.getCurrentSlideCount());
 }
